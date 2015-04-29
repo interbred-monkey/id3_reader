439bccead41fe1e2ef29798d511e485bfe3b18fc
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2015-04-29 12:27:42 +1000

    fix parallel issue #5 by making _instance locally scoped to the object so all the functions can access it without other objects accessing it too

--- includes/tag_extractor.js
+++ includes/tag_extractor.js
@@ -6,88 +6,90 @@ var _       = require('underscore'),
     Buffer  = require('buffer').Buffer;
 
 // include the tag config
-var config      = require('../config/config.json'),
-    _instance   = null;
+var config      = require('../config/config.json');
 
 var tagExtractor = function(tag_data, callback) {
 
-  _instance = this;
+  var _instance = this;
 
-  var tags = _instance.processTags(tag_data.tags);
-  tags.version = tag_data.version;
-  tag_data = tags;
+  this.main = function(tag_data, callback) {
+    var tags = _instance.processTags(tag_data.tags);
+    tags.version = tag_data.version;
+    tag_data = tags;
 
-  return tag_data;
-
-}
-
-tagExtractor.prototype.getUserData = function(tag_text) {
-
-  var return_tag = {};
-
-  for (var cut in config.user_tags) {
-
-    var regex = new RegExp(config.user_tags[cut], 'i');
-
-    if (tag_text.match(regex)) {
-
-      return_tag.label = config.user_tags[cut].toLowerCase().replace(/\s/g, '_');
-      return_tag.text = tag_text.replace(regex, '');
-      break;
-
-    }
-
+    return tag_data;
   }
 
-  return return_tag;
+  this.getUserData = function(tag_text) {
 
-}
+    var return_tag = {};
 
-tagExtractor.prototype.processTags = function(content) {
+    for (var cut in config.user_tags) {
 
-  var tags = {
-    artist: "unknown",
-    title: "unknown",
-    album: "unknown",
-    genre: "unknown"
-  }
+      var regex = new RegExp(config.user_tags[cut], 'i');
 
-  var pos = 10;
+      if (tag_text.match(regex)) {
 
-  while (pos < content.length - 10) {
-    
-    var tag_size = content.readUInt32BE(pos + 4);
-    var tag_label = content.slice(pos, pos + 4).toString('ascii');
-
-    if (_.isUndefined(config.labels[tag_label]) === false) {
-
-      var label = config.labels[tag_label].toLowerCase().replace(/\s/g, '_');
-      var text = content.slice(pos + 10, pos + 10 + tag_size).toString('UTF-8').replace(/[\u0000-\u0009]|~|�/g, '');
-
-      // is this some user defined tag?
-      if (label === "user_defined_text_information_frame") {
-
-        tag_data = _instance.getUserData(text);
-        label = tag_data.label;
-        text = tag_data.text;
-
-      }
-
-      // if we have something in the text then put it in
-      if (!_.isUndefined(label) && !_.isUndefined(text) && text !== "") {
-
-        tags[label] = text;
+        return_tag.label = config.user_tags[cut].toLowerCase().replace(/\s/g, '_');
+        return_tag.text = tag_text.replace(regex, '');
+        break;
 
       }
 
     }
 
-    pos += (tag_size + 10);
+    return return_tag;
 
   }
 
-  return tags;
+  this.processTags = function(content) {
 
+    var tags = {
+      artist: "unknown",
+      title: "unknown",
+      album: "unknown",
+      genre: "unknown"
+    }
+
+    var pos = 10;
+
+    while (pos < content.length - 10) {
+
+      var tag_size = content.readUInt32BE(pos + 4);
+      var tag_label = content.slice(pos, pos + 4).toString('ascii');
+
+      if (_.isUndefined(config.labels[tag_label]) === false) {
+
+        var label = config.labels[tag_label].toLowerCase().replace(/\s/g, '_');
+        var text = content.slice(pos + 10, pos + 10 + tag_size).toString('UTF-8').replace(/[\u0000-\u0009]|~|�/g, '');
+
+        // is this some user defined tag?
+        if (label === "user_defined_text_information_frame") {
+
+          tag_data = _instance.getUserData(text);
+          label = tag_data.label;
+          text = tag_data.text;
+
+        }
+
+        // if we have something in the text then put it in
+        if (!_.isUndefined(label) && !_.isUndefined(text) && text !== "") {
+
+          tags[label] = text;
+
+        }
+
+      }
+
+      pos += (tag_size + 10);
+
+    }
+
+    return tags;
+
+  }
+
+  return this.main(tag_data, callback);
 }
 
 module.exports = tagExtractor;
